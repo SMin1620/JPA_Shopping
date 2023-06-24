4dd1a6ec04a115d05ec1d67a9e998daa099fb373
SMin1620 <ibb09@naver.com>
2023-06-24 17:11:12 +0900

    feat : JPA 와 DB 동작 확인 및 member save, find, test 코드 테스트

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    // JPA 를 사용하기 위한 어노테이션 및 의존선 주입
+    @PersistenceContext
+    private EntityManager em;
+
+    // 유저 저장
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    // 유저 단일 조회
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
