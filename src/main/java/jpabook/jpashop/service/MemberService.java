fe6cacc840e03c3baf6df86e58aa3cd5af3ec10f
SMin1620 <ibb09@naver.com>
2023-07-01 01:39:26 +0900

    feat : 회원 서비스 개발 (create, read)

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);    // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());   // 동시성에서 한번 더 방어를 하기 위해 DB 의 name 컬럼을 유니크 제약을 둔다.
+
+        if (! findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 단일 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
