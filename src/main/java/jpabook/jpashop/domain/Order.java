4234e3992e285535dab4a125cd91bd463c14fa79
SMin1620 <ibb09@naver.com>
2023-07-03 05:43:46 +0900

    feat : 주문, 주문 상품 엔티티 개발(도메인에 기능 메서드 추가) 및 간단한 레포지토리 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,7 +33,7 @@ public class Order {
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private orderStatus status; // 주문 상태 [ORDER, CANCEL]
+    private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
 
 
     ////// 연관 관계 메서드 //////
@@ -52,4 +52,53 @@ public class Order {
         delivery.setOrder(this);
     }
 
+
+    // ==== 생성 메서드 ==== //
+    // order 를 만들면 orderItem이나 delivery도 생성돼야 하기 떄문에, 생성 메서드를 따로 만드는 것이 좋다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+
+    // ==== 비즈니스 로직 ==== //
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+
+    // ==== 조회 로직 ==== //
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+
 }
+
+
+
